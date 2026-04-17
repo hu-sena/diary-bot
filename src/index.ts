a98e56033fb0525eae2ffd462e966a153bcad5b9
husna <shzubir@seekasia.com>
2026-04-17 21:06:24 +0800

    add MessageCreate event

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,8 @@ import {
   Events,
   ChannelType,
 } from "discord.js";
+import { appendToFile } from "./appendToFile";
+import path from "path";
 
 const client = new Client({
   intents: [GatewayIntentBits.DirectMessages, GatewayIntentBits.MessageContent],
@@ -20,7 +22,7 @@ client.on(Events.Raw, async (packet) => {
 
   // skips bot message, this is to avoid infinite loop
   if (data.author?.bot) return;
-
+  if (client.channels.cache.has(data.channel_id)) return;
   try {
     // fetch the channel so discord.js caches it
     const channel = await client.channels.fetch(data.channel_id);
@@ -28,9 +30,25 @@ client.on(Events.Raw, async (packet) => {
     if (!channel || channel.type !== ChannelType.DM) return;
     console.log(`DM from ${data.author.username}: ${data.content}`);
 
-    await channel.send("Got your message!");
+    // await channel.send("Got your message!");
   } catch (error) {
     console.error("Failed to fetch DM channel:", error);
+  }
+});
+
+client.on(Events.MessageCreate, async (message) => {
+  console.log(message);
+  try {
+    if (message.author.bot) return;
+    if (message.author.id !== process.env.DISCORD_USER_ID) return;
+
+    const messageContent = message.content.trim();
+    const filePath = await appendToFile(messageContent);
+
+    await message.reply(`Saved to ${path.basename(filePath)}`);
+  } catch (error) {
+    console.error(error);
+    await message.reply("Failed to save message to file");
   }
 });
 
