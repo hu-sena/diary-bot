3528cfb25203c7fd73307ac8c9fd5b1f7bf7405c
husna <shzubir@seekasia.com>
2026-04-19 17:25:28 +0800

    add writeNotes

---
+++ src/standup/writeNotes.ts
@@ -0,0 +1,16 @@
+import path from "node:path";
+import { getDateString } from "../shared/utils/getDateString";
+import { writeFile } from "node:fs/promises";
+import { createFile } from "../shared/utils/createFile";
+
+const OBSIDIAN_VAULT_PATH = process.env.OBSIDIAN_VAULT_PATH!;
+const FILE_DIRECTORY = path.join(OBSIDIAN_VAULT_PATH, "standup");
+
+export const writeNotes = async (noteContent: string) => {
+  const today = getDateString();
+
+  const filePath = path.join(FILE_DIRECTORY, `${today}.md`);
+
+  await createFile(filePath);
+  await writeFile(filePath, noteContent, "utf-8");
+};
